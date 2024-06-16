ca01dfae6e6bea276e7f6305eb956d1ee879dab4
HelloRWA <dev@rwa-nft.com>
2024-06-16 16:14:47 +0800

    ✨ add bridge pending page

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  extends: [process.env.WEB3_FULL_STACK_LAYER_SAAS_PATH || '@web3-fullstack/layer-saas'],
+  extends: [process.env.WEB3_FULL_STACK_LAYER_SAAS_PATH || "@web3-fullstack/layer-saas"],
   ui: {
-    primary: 'red', // pick from https://tailwindcss.com/docs/customizing-colors#color-palette-reference
-    gray: 'cool'
+    primary: "red", // pick from https://tailwindcss.com/docs/customizing-colors#color-palette-reference
+    gray: "cool",
   },
   colorMode: {
-    preference: 'dark'
-  }
-})
+    preference: "dark",
+  },
+});
---
+++ pages/bridge.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+definePageMeta({
+  colorMode: "dark",
+});
+</script>
+
+<template>
+  <UMain class="flex justify-center items-center">
+    <UContainer>
+      <ULandingSection headline="Stay tuned, coming soon" description="From one chain to anyother chain, whatever, whereever you want.">
+        <template #title> The <span class="text-rainbow animate-pulse uppercase">freedom</span> to moving your assets </template>
+      </ULandingSection>
+      <ULandingGrid>
+        <ULandingCard
+          class="col-span-6 row-span-2"
+          icon="i-material-symbols-light-dynamic-feed-rounded"
+          title="Chain agnosticism"
+          description="No matter what chain, you can bridge."
+        />
+        <ULandingCard class="col-span-6 row-span-4" icon="i-material-symbols-speed" title="Super fast" description="Extreme speed, no lag time" />
+        <ULandingCard class="col-span-6 row-span-4" title="Build via AO " description="Full support for AO, full stack AO">
+          <template #icon>
+            <NuxtImg class="h-8 text-gray-900 w-8 dark:text-white" src="/ao.svg" />
+          </template>
+        </ULandingCard>
+        <ULandingCard
+          class="col-span-6 row-span-2"
+          icon="i-icon-park-outline-many-to-many"
+          title="Any assets"
+          description="ERC20, ERC721, ERC1155, any token, any NFT."
+        />
+      </ULandingGrid>
+    </UContainer>
+  </UMain>
+</template>
