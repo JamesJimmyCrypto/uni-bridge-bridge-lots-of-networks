64f4341a80a1f0d22993c51ceff4ba540868dc99
HelloRWA <dev@rwa-nft.com>
2024-07-12 19:09:58 +0800

    ✨ fix swapkit

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,3 +1,5 @@
+import { nodePolyfills } from "vite-plugin-node-polyfills";
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   extends: [process.env.WEB3_FULL_STACK_LAYER_SAAS_PATH || "@web3-fullstack/layer-saas"],
@@ -8,5 +10,26 @@ export default defineNuxtConfig({
   colorMode: {
     preference: "dark",
     fallback: 'dark'
+  },
+  build: {
+    // transpile: ['@psf/bitcoincashjs-lib']
+  },
+  optimizeDeps: {
+    include: ['@swapkit/sdk > @psf/bitcoincashjs-lib']
+  },
+  ssr: false,
+  vite: {
+    plugins: [
+      nodePolyfills({
+        // Whether to polyfill specific globals.
+        globals: {
+          Buffer: true, // can also be 'build', 'dev', or false
+          global: true,
+          process: true,
+        },
+        // Whether to polyfill `node:` protocol imports.
+        protocolImports: true,
+      }),
+    ],
   },
 });
