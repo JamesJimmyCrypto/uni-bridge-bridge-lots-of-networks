95d8a2db90c22d72db388d21dd4c2de4f2cdc4ac
HelloRWA <dev@rwa-nft.com>
2024-07-29 04:03:19 +0800

    ✨ update form layout

--- components/bridge/InputMenu.vue
+++ components/bridge/InputMenu.vue
@@ -1,43 +1,33 @@
 <script setup lang="ts">
 import type { Avatar } from "#ui/types";
 
-const items = [
-  {
-    id: "benjamincanac",
-    label: "benjamincanac",
-    href: "https://github.com/benjamincanac",
-    target: "_blank",
-    avatar: { src: "https://avatars.githubusercontent.com/u/739984?v=4" },
-  },
-  {
-    id: "Atinux",
-    label: "Atinux",
-    href: "https://github.com/Atinux",
-    target: "_blank",
-    avatar: { src: "https://avatars.githubusercontent.com/u/904724?v=4" },
-  },
-  {
-    id: "smarroufin",
-    label: "smarroufin",
-    href: "https://github.com/smarroufin",
-    target: "_blank",
-    avatar: { src: "https://avatars.githubusercontent.com/u/7547335?v=4" },
-  },
-  {
-    id: "nobody",
-    label: "Nobody",
-    icon: "i-heroicons-user-circle",
-  },
-];
+interface item {
+  id: string;
+  label: string;
+  avatar?: Avatar;
+  icon?: string;
+}
+const { items } = $defineProps<{
+  items: item[];
+}>();
 
-const selected = ref(items[0]);
+let { modelValue } = $defineModels<{
+  modelValue: item;
+}>();
+
+const updateModelValue = (value: item) => {
+  modelValue = value;
+};
 </script>
 
 <template>
-  <UInputMenu v-model="selected" :options="items">
+  <UInputMenu :modelValue="modelValue" @update:modelValue="updateModelValue($event)" :options="items" v-bind="$attrs">
     <template #leading>
-      <UIcon v-if="selected.icon" :name="(selected.icon as string)" class="h-5 w-5" />
-      <UAvatar v-else-if="selected.avatar" v-bind="(selected.avatar as Avatar)" size="2xs" />
+      <div v-if="modelValue" class="flex items-center">
+        <UIcon v-if="modelValue?.icon" :name="(modelValue.icon as string)" class="h-5 w-5" />
+        <UAvatar v-else-if="modelValue?.avatar" v-bind="(modelValue.avatar as Avatar)" size="2xs" />
+      </div>
+      <div v-else></div>
     </template>
   </UInputMenu>
 </template>
--- components/bridge/form.vue
+++ components/bridge/form.vue
@@ -13,18 +13,145 @@ onMounted(async () => {
       value,
     };
   });
-  console.log(`====> tabs :`, tabs);
 });
+
+const allChainList = useSortBy(
+  [
+    {
+      id: "arb",
+      label: "ARB",
+      icon: "token-branded:arbi",
+    },
+    {
+      id: "arweave",
+      label: "Arweave",
+      icon: "token:ar",
+    },
+    {
+      id: "btc",
+      label: "BTC",
+      icon: "token-branded:btc",
+    },
+    {
+      id: "eth",
+      label: "ETH",
+      icon: "token-branded:eth",
+    },
+    {
+      id: "thor",
+      label: "THOR",
+      icon: "token-branded:thor",
+    },
+    {
+      id: "polygon",
+      label: "Polygon",
+      icon: "token-branded:polygon-zkevm",
+    },
+    {
+      id: "polkadot",
+      label: "Polkadot",
+      icon: "token-branded:polkadot",
+    },
+  ],
+  "id"
+);
+
+const fromChainList = [...allChainList];
+let fromChain = $ref();
+
+const fromTokenList = [
+  {
+    id: "benjamincanac",
+    label: "benjamincanac",
+    href: "https://github.com/benjamincanac",
+    target: "_blank",
+    avatar: { src: "https://avatars.githubusercontent.com/u/739984?v=4" },
+  },
+  {
+    id: "Atinux",
+    label: "Atinux",
+    href: "https://github.com/Atinux",
+    target: "_blank",
+    avatar: { src: "https://avatars.githubusercontent.com/u/904724?v=4" },
+  },
+  {
+    id: "smarroufin",
+    label: "smarroufin",
+    href: "https://github.com/smarroufin",
+    target: "_blank",
+    avatar: { src: "https://avatars.githubusercontent.com/u/7547335?v=4" },
+  },
+  {
+    id: "nobody",
+    label: "Nobody",
+    icon: "i-heroicons-user-circle",
+  },
+];
+
+let fromToken = $ref(null);
+let amount = $ref();
+
+const fromWalletAppList = $computed(() => {
+  const walletAppList = [
+    {
+      id: "metamask",
+      label: "MetaMask",
+      icon: "logos:metamask-icon",
+    },
+    {
+      id: "arconnect",
+      label: "ArConnect",
+      avatar: {
+        src: "https://www.arconnect.io/_next/image?url=%2Flogo.png&w=640&q=75",
+      },
+    },
+  ];
+  // TODO: filter out wallet app that not match with current chain
+  return useSortBy(walletAppList, "id");
+});
+
+const fromWalletApp = $ref();
+
+const fromWalletAddressList = ["0xd319905AFEa8401f1eb56fBFD0754853B6B79816", "pSQc9tmQKpw1mqDIOisGL2mXYdRjfldmoLZKkqFhuj4"];
+const fromWalletAddress = $ref();
 </script>
 
 <template>
   <div class="space-y-4 min-w-80">
-    <div class="flex justify-between items-center">
+    <div class="flex-bc">
       <div class="font-bold">From</div>
-      <div class="rounded-lg bg-gray-600 py-2 px-4">0xd31...888</div>
     </div>
-    <div class="rounded-lg bg-gray-600 p-4">
-      <BridgeInputMenu />
+    <div class="rounded-lg space-y-2 bg-gray-600 p-4">
+      <div class="flex-bc space-x-10">
+        <BridgeInputMenu :items="fromChainList" v-model="fromChain" placeholder="Select source chain" />
+        <BridgeInputMenu :items="fromWalletAppList" v-model="fromWalletApp" placeholder="Select wallet app" />
+      </div>
+      <UInputMenu v-model="fromWalletAddress" size="xl" :options="fromWalletAddressList" placeholder="Please select your wallet address" />
+      <div class="flex-bc">
+        <UInput class="flex-1" type="number" size="xl" v-model="amount" placeholder="Please input token amount"></UInput>
+        <BridgeInputMenu :items="fromTokenList" v-model="fromToken" size="xl" />
+      </div>
+
+      <div class="flex-bc px-3">
+        <div>$1223.222</div>
+        <div>Balance: 0.01233 ETH</div>
+      </div>
+    </div>
+    <div class="flex-bc px-4">
+      <div class="font-bold">To</div>
+      <div>arrow icon</div>
+      <div>address</div>
+    </div>
+    <div class="rounded-lg space-y-2 bg-gray-600 p-4">
+      <div class="flex-bc space-x-10">
+        <BridgeInputMenu :items="fromChainList" v-model="fromChain" />
+        <BridgeInputMenu :items="fromTokenList" v-model="fromToken" />
+      </div>
+      <UInput type="number" size="xl" v-model="amount" placeholder="Please input token amount" />
+      <div class="flex-bc px-3">
+        <div>$1223.222</div>
+        <div>Balance: 0.01233 ETH</div>
+      </div>
     </div>
   </div>
 </template>
