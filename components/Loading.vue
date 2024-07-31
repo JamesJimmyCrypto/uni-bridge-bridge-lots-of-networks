58e2df277d0f6c5474c1f201beeb741d63d89137
HelloRWA <dev@rwa-nft.com>
2024-07-31 16:49:10 +0800

    ✨ add loading component

---
+++ components/Loading.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+const { isLoading } = $defineProps<{
+  isLoading: boolean;
+}>();
+</script>
+
+<template>
+  <div v-bind="$attrs">
+    <div v-if="isLoading" class="flex-bc space-x-2">
+      <UIcon name="eos-icons:loading" class="h-5 w-5" />
+    </div>
+    <slot v-else />
+  </div>
+</template>
